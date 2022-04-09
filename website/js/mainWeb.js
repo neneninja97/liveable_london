e064d2a47134c495714fb4a66df9ed48f8028b59
neneninja97 <nene.svoc1997@gmail.com>
2022-04-09 02:32:06 +0200

    initial interactive bit

--- website/js/mainWeb.js
+++ website/js/mainWeb.js
@@ -1,4 +1,3 @@
-var nystories = document.querySelector("p").offsetTop;
 window.onscroll = function() {
   if (window.pageYOffset > 0) {
  var opac = (window.pageYOffset / nystories);
@@ -6,3 +5,11 @@ window.onscroll = function() {
   document.body.style.background = "linear-gradient(rgba(255, 255, 255, " + opac + "), rgba(255, 255, 255, " + opac + ")), url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/times-square-perspective.jpg) no-repeat";
   }
 }
+
+let profSelection = undefined;
+
+document.querySelector("#prof_first_time_renters").addEventListener("click", function(){
+    console.log("clicked");
+    profSelection = "First time renters";
+    console.log(profSelection);
+});
