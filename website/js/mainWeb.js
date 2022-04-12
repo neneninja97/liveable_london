e22e629e182a5f1141e5ba416bedcd4aac1e5af0
Ian Chng <ian@192.168.1.107>
2022-04-12 21:38:54 +0100

    updated website

--- website/js/mainWeb.js
+++ website/js/mainWeb.js
@@ -1,3 +1,11 @@
+var nystories = document.querySelector("p").offsetTop;
+window.onscroll = function() {
+  if (window.pageYOffset > 0) {
+ var opac = (window.pageYOffset / nystories);
+    console.log(opac);
+  document.body.style.background = "linear-gradient(rgba(234, 229, 221, " + opac + "), rgba(234, 229, 221, " + opac + ")), url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/times-square-perspective.jpg) no-repeat";
+  }
+}
 
 Window.onscroll = function() {
   if (window.pageYOffset > 0) {
